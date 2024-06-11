00463c88f5960ad072275e180a5bc7a0ce4f3a09
medDhiaAlaya <dhiaalaya3@gmail.com>
2024-06-11 18:54:18 +0100

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:barista/firebase_options.dart';
 import 'package:barista/presentation/home/bloc/home_bloc.dart';
 import 'package:barista/presentation/home/home.dart';
 import 'package:barista/presentation/on_boarding/on_boarding_screen.dart';
 import 'package:barista/shared/network/local/cache_helper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   await CacheHelper.init();
   bool? onBoarding = await CacheHelper.getData(key: 'onboarding');
   final Widget initialWidget;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,11 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import connectivity_plus
 import file_selector_macos
+import firebase_core
+import firebase_storage
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
@@ -14,8 +17,11 @@ import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
